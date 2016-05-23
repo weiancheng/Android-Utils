76b42598587fca3b7e188784a3e937360f7cb1b7
Weian Cheng <weian_cheng@htc.com>
2016-05-23 16:06:57 +0800

    add logcat.

---
+++ logcat/logcat.h
@@ -0,0 +1,7 @@
+#include <android/log.h>
+
+#define LOGV(…) __android_log_print(ANDROID_LOG_VERBOSE, "Test", __VA_ARGS__)
+#define LOGD(…) __android_log_print(ANDROID_LOG_DEBUG, "Test", __VA_ARGS__)
+#define LOGI(…) __android_log_print(ANDROID_LOG_INFO, "Test", __VA_ARGS__)
+#define LOGW(…) __android_log_print(ANDROID_LOG_WARN, "Test", __VA_ARGS__)
+#define LOGE(…) __android_log_print(ANDROID_LOG_ERROR, "Test", __VA_ARGS__)
